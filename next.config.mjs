0b31cf75395f0a0424abd728441b314cb9a52d73
jamnorth <james.north@live.com.au>
2026-02-06 21:25:01 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  webpack: (config) => {
+    config.resolve.alias["@"] = process.cwd();
+    return config;
+  },
 };
 
 export default nextConfig;
