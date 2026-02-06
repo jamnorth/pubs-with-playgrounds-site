d74b847e767c37b3e77913f8bde3748080b6ae36
jamnorth <james.north@live.com.au>
2026-02-06 21:37:51 +1000

    Update route.ts

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -1,9 +1,8 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2026-01-28.clover",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
 
 const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 
